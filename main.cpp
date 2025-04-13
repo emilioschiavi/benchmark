1d5a372a56b1f6daf5957c6803e1ca849f77ed45
Emilio Schiavi <emilioschiavi@hotmail.com>
2025-04-13 16:05:53 +0200

    added benchmarks

--- main.cpp
+++ main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#include <unordered_map>
+#include <string>
 #include <benchmark/benchmark.h>
 
 constexpr size_t DATA_SIZE = 10000;
+constexpr size_t MAP_SIZE = 1000000;
+
+std::unordered_map<std::string, int32_t> s_unordered_map;
+std::map<std::string, int32_t> s_map;
+
+void createMaps()
+{
+    for (size_t i = 0; i < MAP_SIZE; i++)
+    {
+        s_unordered_map[std::to_string(i)] = i;
+        s_map[std::to_string(i)] = i;
+    }
+}
+
+static void DoSetup([[maybe_unused]] const benchmark::State &state)
+{
+    static bool callSetup = true;
+    if (callSetup)
+    {
+        createMaps();
+        callSetup = false;
+    }
+}
 
 void slowFunction()
 {
@@ -28,6 +54,24 @@ void stlFunction()
     int32_t counter = 0;
     std::generate(data.begin(), data.end(), [&]()
                   { return counter++; });
+}
+
+void mapFnc()
+{
+    const auto &it1 = s_map.find("1");
+    if (it1->second != 1)
+    {
+        assert(false);
+    }
+}
+
+void unordered_mapFnc()
+{
+    const auto &it1 = s_unordered_map.find("1");
+    if (it1->second != 1)
+    {
+        assert(false);
+    }
 }
 
 static void BM_slowFunction(benchmark::State &state)
@@ -60,10 +104,32 @@ static void BM_stlFunction(benchmark::State &state)
     }
 }
 
+static void BM_map(benchmark::State &state)
+{
+    // Perform setup here
+    for (auto _ : state)
+    {
+        // This code gets timed
+        mapFnc();
+    }
+}
+
+static void BM_unordered_map(benchmark::State &state)
+{
+    // Perform setup here
+    for (auto _ : state)
+    {
+        // This code gets timed
+        unordered_mapFnc();
+    }
+}
+
 // Register the functions as a benchmark
 BENCHMARK(BM_slowFunction);
 BENCHMARK(BM_fastFunction);
 BENCHMARK(BM_stlFunction);
+BENCHMARK(BM_map)->Setup(DoSetup);
+BENCHMARK(BM_unordered_map)->Setup(DoSetup);
 
 // Run the benchmark
 BENCHMARK_MAIN();
